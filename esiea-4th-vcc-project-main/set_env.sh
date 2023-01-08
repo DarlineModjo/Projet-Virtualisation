c921717296719a734e1121a7d9a09b94252434a8
DarlineModjo <darlinemodjo@gmail.com>
2023-01-08 23:59:24 +0100

    Projet Virtualisation vf2

---
+++ esiea-4th-vcc-project-main/set_env.sh
@@ -0,0 +1,13 @@
+
+
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=adminpost
+export POSTGRES_PASSWORD=postgresADY
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-esiea-ady-01019
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
